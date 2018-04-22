6af679559a0ae3f0a5155e85bca5fffddc362bb8
Kaj Białas <kaj.bialas@dev-industry.com>
2018-04-22 10:55:22 +0200

    Render class

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
-var personDb = [];
+// Class DB
+function PersonDB() {
+  this.database = [];
+};
 
+PersonDB.prototype.addPerson = function(person) {
+  this.database.push(person);
+};
+
+PersonDB.prototype.displayPeople = function() {
+  console.log(this.database);
+};
+
+
+// Class People
 function Person(attributes) {
   this.name = attributes.name;
   this.surname = attributes.surname;
   this.age = attributes.age;
   this.role = attributes.role;
 };
+
+
+// Class for render methods
+function Render(attribute) {
+  this.container = document.getElementById(attribute);
+};
+
+Render.prototype.renderRow = function(name, surname, age, role) {
+  return '<div><div>' + name + '</div><div>' + surname + '</div><div>' + age + '</div><div>' + role + '</div></div>';
+};
+
+Render.prototype.setHtml = function(database) {
+  console.log('database', this.container);
+  this.container.innerHTML = '';
+  var self = this;
+  database.map(function(person){
+    self.container.innerHTML += this.renderRow(person.name, person.surname, person.age, person.role);
+  });
+};
+
+// Create object of PersonDB
+var dbInstance = new PersonDB();
+
+// Create object of Render
+var renderInstance = new Render('records');
+
+
+renderInstance.setHtml(dbInstance.database);
+
+
+
+
+
+
+
+
+
+
+
 
 function renderRow(name, surname, age, role) {
   return '<div><div>' + name + '</div><div>' + surname + '</div><div>' + age + '</div><div>' + role + '</div></div>';
@@ -14,7 +66,7 @@ function renderRow(name, surname, age, role) {
 function renderDb() {
   var containerDb = document.getElementById('records');
   containerDb.innerHTML = '';
-  personDb.map(function(person){
+  dbInstance.database.map(function(person){
     containerDb.innerHTML += renderRow(person.name, person.surname, person.age, person.role);
   });
 };
@@ -35,10 +87,9 @@ addButton.addEventListener('click', function(){
     role: inputRole
   });
 
-  personDb.push(person1);
-
-  renderDb();
+  dbInstance.addPerson(person1);
+  console.log('click', dbInstance.database);
+  renderInstance.setHtml(dbInstance.database);
 });
 
 
-renderDb();
